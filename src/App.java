2d8285115abfe61575aa4c03a2fba50ba1bb790f
MarekJakimiuk1 <marekjakimiuk1@gmail.com>
2016-11-17 18:20:30 +0100

    Add view class constructor and basic frame.

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
+import java.awt.EventQueue;
 
 public class App {
 
 	public static void main(String[] args) {
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View window = new View();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
-
 }
--- src/View.java
+++ src/View.java
@@ -1,4 +1,18 @@
+import javax.swing.JFrame;
 
 public class View {
+		
+		JFrame frame;
 
-}
+		public View() {
+			initialize();
+		}
+		
+		private void initialize() {
+			frame = new JFrame();
+			frame.setBounds(250, 150, 400,400);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+	}
+
+
